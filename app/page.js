e1c46838dfd62de8de30c92bf38be70d901c4714
Yushu0 <119829083+Yushu0@users.noreply.github.com>
2023-12-11 22:46:51 -0700

    a little bit changes

--- app/page.js
+++ app/page.js
@@ -5,6 +5,33 @@ import styles from "./page.module.css";
 import search from "./assets/search.svg";
 import { useEffect, useState } from "react";
 
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = '86635fd29966e41830e4cc8e9670aa79';
+const UNITS = 'metric';
+
+const WeatherInfo = ({ data }) => (
+  <div className={styles.weather}>
+    {data.name && <p>City: {data.name}</p>}
+    {data.main && <p>Temperature: {data.main.temp}</p>}
+    {data.main && <p>Pressure: {data.main.pressure}</p>}
+    {data.main && <p>Humidity: {data.main.humidity}</p>}
+    {data.wind && <p>Wind Speed: {data.wind.speed}</p>}
+  </div>
+);
+
+const SearchForm = ({ onChange, onSubmit }) => (
+  <div className={styles.search}>
+    <input
+      className={styles.search_bar}
+      placeholder="Search your city name"
+      onChange={onChange}
+    />
+    <button onClick={onSubmit}>
+      <Image src={search} alt="search" width={24} height={24} />
+    </button>
+  </div>
+);
+
 export default function Home() {
   const [data, setData] = useState([]);
   const [city, setCity] = useState("");
@@ -13,15 +40,17 @@ export default function Home() {
     setCity(e.target.value);
   };
 
-  const handleSubmit = () => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=86635fd29966e41830e4cc8e9670aa79&units=metric`
-    )
-      .then((res) => res.json())
-      .then((res) => setData(res));
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch(
+        `${API_URL}?q=${city}&appid=${API_KEY}&units=${UNITS}`
+      );
+      const weatherData = await response.json();
+      setData(weatherData);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
   };
-
-  console.log(data);
 
   useEffect(() => {
     handleSubmit();
@@ -30,23 +59,8 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <div className={styles.search}>
-          <input
-            className={styles.search_bar}
-            placeholder="Search your city name"
-            onChange={handleChange}
-          />
-          <button onClick={handleSubmit}>
-            <Image src={search} alt="search" width={24} height={24} />
-          </button>
-        </div>
-        <div className={styles.weather}>
-          {data.name && <p>City: {data.name}</p>}
-          {data.main && <p>Temperature: {data.main.temp}</p>}
-          {data.main && <p>Pressure: {data.main.pressure}</p>}
-          {data.main && <p>Humidity: {data.main.humidity}</p>}
-          {data.wind && <p>Wind Speed: {data.wind.speed}</p>}
-        </div>
+        <SearchForm onChange={handleChange} onSubmit={handleSubmit} />
+        <WeatherInfo data={data} />
       </div>
     </main>
   );
